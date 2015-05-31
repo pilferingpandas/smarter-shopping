afe3597ec86f44dbd60c880cbb2c661f166e94e2
Emily Shenfield <emily.shenfield@gmail.com>
2015-05-30 19:46:40 -0700

    dev(server): working on adding Items and getting a List in the listController

--- server/lists/listController.js
+++ server/lists/listController.js
@@ -8,8 +8,18 @@ var User = mongoose.model('User', models.user);
 module.exports = {
 
   createUser: function() {
-    User.create({username:'emily', list:[], past_items:[]}, function(err, newUser) {
-      console.log("User created! Welcome: ", newUser.username);
+    var user = new User({username:'emily', list:[], past_items:[]})
+
+    User.find({username: 'emily'}, function(err, users) {
+      if (err) console.error(err);
+      console.log(users);
+      if (users.length > 0) {
+        console.log('This user already exists!')
+      } else {
+        User.create(user, function(err, newUser) {
+          console.log('User created! Welcome: ', newUser.username);
+        });
+      }
     });
   },
   
@@ -18,18 +28,22 @@ module.exports = {
     var findUser = Q.nbind(User.findOne, User);
     findUser({username: username})
     .then(function(user) {
-      user
-        .populate('list')
-        .exec(function(err, user) {
-          res.json(user.list);
-        });
+      console.log('Found user! ', user);
+      res.send(user.list);
+        // .populate('list')
+        // .exec(function(err, user) {
+        //   if (err) console.error(err);
+        //   console.log('Inside exec')
+        //   res.json(user.list);
+        // });
     });
   },
 
   addItem: function(req, res) {
+    var username = 'emily';
     var name = req.body.name;
     var frequency = req.body.frequency;
-    var newItem = {
+    var item = new Item({
       name: name,
       data: {
         frequency: frequency,
@@ -37,9 +51,9 @@ module.exports = {
         food_category: 'none',
         expiration: new Date(15,9,16)
       }
-    };
+    });
     var createItem = Q.nbind(Item.create, Item);
-    var findItem = Q.nbind(Item.find, Item);
+    var findItem = Q.nbind(Item.findOne, Item);
     var findUser = Q.nbind(User.findOne, User);
 
     findItem({name : name})
@@ -47,17 +61,33 @@ module.exports = {
       if (match) {
         findUser({username: username})
         .then(function(user) {
-          user.list.push(match)
+          User.findByIdAndUpdate(
+            user._id,
+            {$push: {'list': createdItem._id}},
+            {safe:true, upsert:true},
+            function(err, model) {
+              if(err) console.log(err);
+              console.log("Updated model!", model);
+            }
+          );
         });
         res.send(match);
       } else {
-        return createItem(newItem)
+        return createItem(item)
       }
     })
     .then(function(createdItem) {
       findUser({username: username})
       .then(function(user) {
-        user.list.push(createdItem._id);
+        User.findByIdAndUpdate(
+          user._id,
+          {$push: {'list': createdItem._id}},
+          {safe:true, upsert:true},
+          function(err, model) {
+            if(err) console.log(err);
+            console.log("Updated model!", model);
+          }
+        );
       });
       res.send(createdItem);
     });
