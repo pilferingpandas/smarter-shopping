3d12b6a0b0b14df7eaa7ace9ece134c219b1073b
Alex <aj42869@gmail.com>
2015-06-05 10:23:35 -0700

    dev(client): created Login and Register api ajaxs requests

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,7 +4,15 @@ var Register = React.createClass({
   render: function() {
     return (
       <div id="register">
-        Register
+        <h2> Register </h2>
+        <form onSubmit={this.handleSubmit}>
+          <label><input ref="email" placeholder="email" /></label>
+          <label><input ref="pass" placeholder="password"/></label>
+          <button type="submit">login</button>
+          {this.state.error && (
+            <p>Bad login information</p>
+          )}
+        </form>
       </div>
     );
   }
