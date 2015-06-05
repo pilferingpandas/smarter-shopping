e95e428c1557d6491648680e88d3a95c1aa0e3d0
Alex <aj42869@gmail.com>
2015-06-05 10:12:49 -0700

    refactor(client): Added ajax call to the client for login post requests

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,37 @@
 var React = require('react');
 
 var Login = React.createClass({
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+    var email = this.refs.email.getDOMNode().value;
+    var password = this.refs.email.getDOMNode().value;
+    var loginUrl = url + '/api/login';
+    $.post('/api/login', function() {
+      url: ,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        console.log(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({ error: true })
+        console.error(loginUrl, status, err);
+      }.bind(this)
+    })
+  },
+
   render: function() {
     return (
       <div id="login">
-        Login
+        <form onSubmit={this.handleSubmit}>
+          <label><input ref="email" placeholder="email" defaultValue="joe@example.com"/></label>
+          <label><input ref="pass" placeholder="password"/></label>
+          <button type="submit">login</button>
+          {this.state.error && (
+            <p>Bad login information</p>
+          )}
+        </form>
       </div>
     );
   }
