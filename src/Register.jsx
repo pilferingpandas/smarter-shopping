eb458430d79878209b0bdda488054c4065909dec
Joe Prempeh <prempehjoe@gmail.com>
2015-06-08 15:36:46 -0700

    style(login/reg): Login and register styled

--- src/Register.jsx
+++ src/Register.jsx
@@ -14,14 +14,26 @@ var Register = Eventful.createClass({
 
   render: function() {
     return (
-      <div id="register">
-        <h2>Register</h2>
-        <form className="register-form" onSubmit={this.registerUser}>
-          <input ref="username" placeholder="username" />
-          <input ref="password" placeholder="password" />
-          <button type="submit">Register</button>
+    <div id="register" className="middle-box text-center loginscreen  animated fadeInDown">
+      <div>
+        <div>
+          <h2 className="logo-name"><i className="fa fa-cart-plus fa-spin"></i><br />SSL</h2>
+        </div>
+        <h3>Welcome to Smart Shopping List</h3>
+        <p>Register</p>
+        <form className="register-form" name="registerform" onSubmit={this.registerUser}>
+          <div className="form-group">
+            <i className="fa fa-user-plus fa-lg"></i>&nbsp;&nbsp;&nbsp;<input ref="username" placeholder="Username" />
+          </div>
+          <div className="form-group">
+            <i className="fa fa-key fa-lg"></i>&nbsp;&nbsp;&nbsp;<input ref="password" placeholder="Password" type="password" />
+          </div>
+          <button type="submit" className="btn btn-success btn-block" ><i className="fa fa-user-plus fa-lg"></i>&nbsp;&nbsp;&nbsp;Register</button>
+          <p className="text-muted text-center"><small>If you already have an account...</small></p>
+          <a className="btn btn-sm btn-primary btn-block" href="#/login"><i className="fa fa-user fa-lg"></i>&nbsp;&nbsp;&nbsp;Login</a>
         </form>
       </div>
+    </div>
     );
   }
 });
