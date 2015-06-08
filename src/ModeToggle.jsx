c2037ac3e9ada8130883150be970019af8c3fc39
Joe Prempeh <prempehjoe@gmail.com>
2015-06-08 11:59:48 -0700

    dev(modetoggle): mode toggle buttons styled

--- src/ModeToggle.jsx
+++ src/ModeToggle.jsx
@@ -20,11 +20,23 @@ var ModeToggle = Eventful.createClass({
   render: function() {
     var mode = this.state.mode === ModeToggle.SHOPPING ? 'SHOPPING' : 'EDITING';
     return (
-      <div id="mode-toggle">
-        <form>
-          <input type="radio" name="shopping" value="SHOPPING" />Shopping Mode
-          <input type="radio" name="editing" value="EDITING" />Editing Mode
-        </form>
+      <div className="ibox-content">
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <div id="mode-toggle">
+            <div className="ibox float-e-margins">
+            <div data-toggle="buttons" className="btn-group">
+              <form>
+                <label className="btn btn-sm btn-white"> <input type="radio" name="shopping" value="SHOPPING" />&nbsp;&nbsp;&nbsp;Shopping Mode</label>
+                <label className="btn btn-sm btn-white active"> <input type="radio" name="editing" value="EDITING" />&nbsp;&nbsp;&nbsp;Editing Mode</label>
+              </form>
+            </div>
+          </div>
+            </div>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
       </div>
     );
   }
