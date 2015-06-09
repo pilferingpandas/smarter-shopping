210e3d73ca848a7e85486bfd74dac54de01a684e
TheRealest <real@rrprovencher.com>
2015-06-08 22:02:46 -0700

    dev(suggestions): finished first half of calculateSuggestions function

--- server/lists/suggestionController.js
+++ server/lists/suggestionController.js
@@ -5,9 +5,56 @@ var Item = mongoose.model('Item', models.item);
 var User = mongoose.model('User', models.user);
 var Suggestion = mongoose.mode('Suggestion', models.suggestion);
 
-
+var findUser = Q.nbind(User.findOne,User);
 
 var calculateSuggestions = function(uid) {
+  return findUser({usename: uid})
+  .then(function(user) {
+    return user.past_items.reduce(function(item, past_items) {
+      var iid = item.item_id;
+      var date = item.purchase_dates[0];
+      if (past_items[iid]) {
+        past_items[iid].push(date);
+      } else {
+        past_items[iid] = [date];
+      }
+      return past_items;
+    },{});
+  })
+  .then(function(past_items) {
+    var pastItemOccurrences = {};
+    for (var iid in past_items) {
+      pastItemOccurrences[iid] = past_items[iid];
+    }
+
+    for (var iid in past_items) {
+      var dates = past_items[iid];
+      if (dates.length <= 1) delete past_items[iid];
+      var intervals = [];
+      for (var i = 1; i < dates.length; i++) {
+        intervals[i-1] = new Date(dates[i]) - new Date(dates[i-1]);
+      }
+      past_items[iid] = intervals;
+    }
+    return [past_items, pastItemOccurrences];
+  })
+  .spread(function(pastItemIntervals, pastItemOccurrences) {
+    for (var iid in past_items) {
+      var sum = past_items[iid].reduce(function(interval,sum) {
+        return sum + interval;
+      },0);
+      var avg = sum / past_items[iid].length;
+      past_items[iid] = avg;
+    }
+    return [past_items, pastItemOccurrences];
+  })
+  .spread(function(pastItemFrequencies, pastItemOccurrences) {
+
+  })
+  .then(function(pastItemOccurrences) {
+  })
+
+  //    past_items          ([item_id: oid, purchase_dates: [Date]]) -> map
   //    past_items          ({oid: [Date]}) -> map
   // -> pastItemIntervals   ({oid: [Int]})  -> map
   // -> pastItemFrequencies ({oid: Float})  -> map
@@ -16,7 +63,7 @@ var calculateSuggestions = function(uid) {
   // -> itemIsPredicted     ({oid: Bool})   -> filter
   // -> predictedItems      ([oid])         -> filter w/ user.past_items ([oid])
   // -> itemSuggestions     ([oid])         -> take 2
-  // -> respond.send()
+  // -> fill user.suggestions
 };
 
 module.exports = {
@@ -27,7 +74,7 @@ module.exports = {
     .populate('suggestions')
     .exec(function(err, user) {
       if (err) console.error(err);
-      console.log('in get list, user:',user);
+      console.log('in get suggestions, user:',user);
       res.send(user.suggestions);
     });
   },
