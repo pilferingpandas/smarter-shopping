e6668080dcb89dd23af3c51f822ab250e2d76585
David Goldberg <drgold1988@gmail.com>
2015-06-10 20:04:45 -0700

    feat(followerList): adds list of followers returned from server

--- server/lists/feedController.js
+++ server/lists/feedController.js
@@ -5,11 +5,26 @@ var Item = mongoose.model('Item', models.item);
 var User = mongoose.model('User', models.user);
 
 var checkUserExists = function(user, cb) {
+
+  // TODO: if already in following do not add again
+
   User.find({username: user}, function (err, docs) {
+    console.log('docs: ', docs);
     if (docs.length) {
       cb(true);
     } else {
       cb(false);
+    }
+  }); 
+};
+
+var getFollowerList = function(req, res) {
+  var currentUser = req.uid;
+  User.find({username: currentUser}, function (err, docs) {
+    console.log('docs: ', docs);
+    if (docs.length) {
+      var following = docs[0].following;
+      res.send(following);
     }
   }); 
 };
@@ -32,7 +47,7 @@ var addFollower = function(req, res) {
             console.error(err);
             res.status(500).send({ error: 'Server Error'});
           } else {
-            res.send("Success");
+            getFollowerList(req, res);
           }
         });
     } else {
@@ -44,11 +59,26 @@ var addFollower = function(req, res) {
 };
 
 var removeFollower = function() {
+  // Allows users to unfollow other users once they are following them
+};
 
+var getFollowerItems = function() {
+  // Renders follower items into the news feed when they are added to your followers lists
+
+};
+
+var addFollowerItem = function() {
+  // Adds an individual item from the news feed to your list when you click on the "like" icon for a particular item
 };
 
 module.exports = {
   addFollower: addFollower,
   removeFollower: removeFollower,
-  checkUserExists: checkUserExists
+  checkUserExists: checkUserExists,
+  getFollowerList: getFollowerList,
+  getFollowerItems: getFollowerItems,
+  addFollowerItem: addFollowerItem
 };
+
+
+  
